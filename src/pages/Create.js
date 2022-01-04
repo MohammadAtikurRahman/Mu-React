93edd29db966d8ff91f289288050342882b2c2b0
Md. Atikur  Rahman <contact.atikurrahman@gmail.com>
2022-01-04 14:55:54 +0600

    added

---
+++ src/pages/Create.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import  Typography  from '@mui/material/Typography'
+
+
+export default function Create() {
+  return (
+    <div>
+
+       <Typography 
+       variant="h1" 
+       color="primary"
+       align = "center"
+       
+       >
+
+            Future Innovation
+
+       </Typography>
+        <Typography 
+       variant="h2" 
+       color="secondary"
+       align = "center"
+       
+       >
+
+           Living with the techonology
+
+       </Typography>
+
+
+ <Typography 
+       variant="h3" 
+      color="primary"
+      noWrap
+    
+       
+       >
+
+         Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore esse quaerat voluptatum repellat vero at eaque aut molestias minus expedita quos temporibus veritatis odio asperiores, debitis reprehenderit iusto excepturi a.
+
+       </Typography>
+
+
+
+    </div>
+  )
+}
---
+++ src/pages/Notes.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Typography from '@mui/material/Typography';
+
+export default function Notes() {
+  return (
+    <div>
+      Notes page
+    </div>
+  )
+}
