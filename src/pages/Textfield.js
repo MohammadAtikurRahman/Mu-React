b88c6f0e9e81d1d5f2ea80e05a777ed5bbefb74c
Md. Atikur  Rahman <contact.atikurrahman@gmail.com>
2022-01-11 02:15:01 +0600

    textfiledvalue

--- src/pages/Textfield.js
+++ src/pages/Textfield.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React ,{ useState } from 'react'
 import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
 import Container from '@mui/material/Container';
@@ -21,32 +21,76 @@ const useStyles = makeStyles({
 export default function Notes() {
  
      const classes = useStyles()
+     const [title,setTitle] =useState('')
+     const [details,setDetails] =useState('')
+     const [titleError,setTitleError] =useState('false')
+     const [detailsError,setDetailsError] =useState('false')
+
+
+
+      const handleSubmit = (e) => {
+       
+       
+         e.preventDefault();
+          setTitleError(false)
+          setDetailsError(false)
+
+          if(title == ''){
+
+            setTitleError(true)
+          }
+
+          if(details == ''){
+
+            setDetailsError(true)
+          }
+          if(title && details){
+
+            console.log(title,details)
+          }
+      }
  
     return (
     <Container>
 
-        <Typography variant="h5" color="initial"
-        
-       pt={2}
-        
-        >
+        <Typography variant="h5" color="initial"  pt={2}>
             Create a Note
         </Typography>
-         <form noValidate autoComplete="off">
 
-       
+
+         <form noValidate autoComplete="off" onSubmit={handleSubmit} >
            <TextField
-              className = {classes.field}
+             onChange={(e) => setTitle(e.target.value)}
+
+                
+            
+             className={classes.field}
              label="Note Title"
              variant="outlined"
              color= "secondary"
              fullWidth
              required
-         
-             
+             error={titleError}
            />
+              <TextField
 
-           <Button variant="contained" color="success">
+               onChange={(e) => setDetails(e.target.value)}
+             className = {classes.field}
+             label="Note Title"
+             variant="outlined"
+             color= "secondary"
+             multiline
+             rows={4}
+             fullWidth
+             required
+                error={detailsError}
+           />
+            
+
+           <Button 
+           type= "submit"
+           variant="contained" 
+           color="success">
              Send
            </Button>
 
